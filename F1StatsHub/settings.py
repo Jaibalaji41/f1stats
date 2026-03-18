fcb9a8d2448720566b9e5bbf7d85b3895a600af6
Jaibalaji41 <jaibalaji10c@gmail.com>
2026-03-18 14:38:28 +0530

    Deploy to Vercel: Added vercel.json, requirements.txt, and updated settings for Whitenoise

--- F1StatsHub/settings.py
+++ F1StatsHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i5c-l)8h6s@^uphu$-@qajj)5*3xug$dv&*_h=no(c0^22nky*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise storage to compress and cache static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files setup
 MEDIA_URL = '/media/'

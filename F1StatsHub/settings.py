2c48915445c6d780eb41685dd06915fd40d2be76
Jaibalaji41 <jaibalaji10c@gmail.com>
2026-03-18 15:08:11 +0530

    Deploy: Professional static file handling with WhiteNoise ManifestStorage and collectstatic for Vercel

--- F1StatsHub/settings.py
+++ F1StatsHub/settings.py
@@ -116,13 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# WhiteNoise configuration for serverless deployment
-# CompressedStaticFilesStorage works without needing a manifest.json
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# WhiteNoise storage to compress and cache static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
 
 # Disable DEBUG in production (Vercel)
